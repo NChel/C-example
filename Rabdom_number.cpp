8a97ed2339d623991b2c8a74738cf917edf7e4c5
BC <waltenp@gmail.com>
2018-07-18 12:28:29 +0900

    c++ Random

---
+++ Rabdom_number.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstring>
+#include <ctime>
+using namespace std;
+
+int main(void) {
+	srand(time(NULL));
+	for (int i = 0; i < 5; i++) {
+		cout << "Random number : " << rand() % 100 << endl;
+	}
+	return 0;
+}
